ff59d68a10b92bfc230d89a3ad2f8871504a24e7
Dipanshu <deepanshu.patil69@gmail.com>
2023-11-30 17:38:34 +0530

    playlist model

---
+++ server/src/models/playList.ts
@@ -0,0 +1,42 @@
+import { Model, ObjectId, Schema, model, models } from "mongoose";
+
+interface PlayListDocument {
+  title: string;
+  owner: ObjectId;
+  items: ObjectId[];
+  visibility: "public" | "private" | "auto";
+}
+
+const playListSchema = new Schema<PlayListDocument>(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    owner: {
+      type: String,
+      required: true,
+      ref: "User",
+    },
+    items: [
+      {
+        type: String,
+        required: true,
+        ref: "Audio",
+      },
+    ],
+    visibility: {
+      type: String,
+      enum: ["public", "private", "auto"],
+      default: "public",
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const PlayList = models.PlayList || model("PlayList", playListSchema);
+
+export default PlayList as Model<PlayListDocument>;
